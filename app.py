7cabaa2420513568b9a3ab25db6651a2fd5054cc
Christopher Cox <christophercox@MacBook-Pro-5.fios-router.home>
2024-05-01 22:11:58 -0400

    Updating license to GPL

--- app.py
+++ app.py
@@ -1,3 +1,20 @@
+# Copyright (c) 2023-2024 Westfall Inc.
+#
+# This file is part of Windweaver.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, and can be found in the file NOTICE inside this
+# git repository.
+#
+# This program is distributed in the hope that it will be useful
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import os
 import secrets
 from urllib.parse import urlencode
